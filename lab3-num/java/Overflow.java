e75fd92c63d904a9d46a5cfbd75bf05dbc206180
Benedict P-H <220160650@aston.ac.uk>
2024-02-11 22:37:05 +0000

    Lab 3

--- lab3-num/java/Overflow.java
+++ lab3-num/java/Overflow.java
@@ -86,7 +86,7 @@ public class Overflow
         for ( int j = 1; j <= m; j++ )
         {
             ns = ns * n;
-//             assert ?? : "Floating-point overflow";
+            assert ns <= 3.4028235E38 : "Floating-point overflow";
             resultList.add(ns);
         }
         return resultList;
@@ -112,7 +112,7 @@ public class Overflow
         for ( int j = 0; j <= m; j++ )
         {
             ns_inv = ns_inv / n; // update from 1/n^(m-1) to 1/n^m
-            assert ns_inv > 0 : "Floating point underflow";
+            assert ns_inv >= 0.00000001 : "Floating point underflow";
             geom_sum = ns_inv + geom_sum;
             resultList.add(geom_sum);
         }
